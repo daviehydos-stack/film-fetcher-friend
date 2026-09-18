3e950319ba96a9c8ce6576fcbc3a613a26f07509
daviehydos-stack <davie.hydos@gmail.com>
2026-09-18 08:36:57 +0300

    feat: connect official Avant YouTube channel and preview sources

--- src/lib/site-data.ts
+++ src/lib/site-data.ts
@@ -3,7 +3,7 @@ export const WHATSAPP_TEL = "tel:+254720814693";
 
 export const socials = {
   facebook: "https://www.facebook.com/AvantFilms/",
-  youtube: "https://www.youtube.com/channel/UCq7Uyw6knbdJi6WuGF2lYdw",
+  youtube: "https://www.youtube.com/@avantcinemas",
   instagram: "https://www.instagram.com/jgatero/?hl=en",
   twitter: "http://www.twitter.com/wix",
 };
@@ -30,7 +30,7 @@ export type CatalogueTitle = {
   legacyPath: string;
   featured?: boolean;
   available?: boolean;
-  trailerEmbedUrl?: string;
+  trailerEmbedUrl?: string;\n  previewYoutubeId?: string;
   episodes?: Episode[];
 };
 
@@ -361,7 +361,7 @@ export const catalogue: CatalogueTitle[] = [
     synopsis: "A governor's son and a girl from the slum meet in a city that never lets anyone forget where they come from.",
     artwork: "https://static.wixstatic.com/media/57086b_a90ca602545d4841be33a5759cffc808~mv2.png/v1/fill/w_930,h_523,enc_auto/file.png",
     backdrop: "https://static.wixstatic.com/media/57086b_a90ca602545d4841be33a5759cffc808~mv2.png/v1/fill/w_1834,h_1032,enc_auto/file.png",
-    legacyPath: "/a-better-life", episodes: [...betterLifeFree, ...betterLifePaid],
+    legacyPath: "/a-better-life", previewYoutubeId: "LzKnWnEIr1U", episodes: [...betterLifeFree, ...betterLifePaid],
   },
   {
     id: "this-is-life", slug: "this-is-life", title: "This is Life", type: "series",
@@ -370,7 +370,7 @@ export const catalogue: CatalogueTitle[] = [
     synopsis: "An intimate series about love, work, difficult choices and the lives built in between.",
     artwork: "https://static.wixstatic.com/media/57086b_899d3183eb3c450e96b8ae328886165b~mv2.png/v1/fill/w_900,h_506,enc_auto/file.png",
     backdrop: "https://static.wixstatic.com/media/57086b_46bd84f76e9e40a0bfad5e2852bf6b09~mv2.png/v1/fill/w_1834,h_1032,enc_auto/file.png",
-    legacyPath: "/this-is-life", episodes: [...thisIsLifeFree, ...thisIsLifePaid],
+    legacyPath: "/this-is-life", previewYoutubeId: "JyqgBodtgbY", episodes: [...thisIsLifeFree, ...thisIsLifePaid],
   },
   {
     id: "better-days", slug: "better-days", title: "Better Days", type: "series",
