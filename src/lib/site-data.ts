61f8c9c7377653ea8121848cb0f3e56ed2937664
daviehydos-stack <davie.hydos@gmail.com>
2026-09-18 07:55:22 +0300

    Integrate streaming UI: src/lib/site-data.ts

--- src/lib/site-data.ts
+++ src/lib/site-data.ts
@@ -2,10 +2,10 @@ export const WHATSAPP = "+254720814693";
 export const WHATSAPP_TEL = "tel:+254720814693";
 
 export const socials = {
-  facebook: "https://www.facebook.com/AvantFilms/",
-  youtube: "https://www.youtube.com/channel/UCq7Uyw6knbdJi6WuGF2lYdw",
-  instagram: "https://www.instagram.com/jgatero/?hl=en",
-  twitter: "http://www.twitter.com/wix",
+  facebook: "https://www.facebook.com/avantfilmskenya/",
+  youtube: "https://www.youtube.com/@avantmoviesforusbyus",
+  instagram: "https://www.instagram.com/avantmovieske?igsh=dGJmZjZzeTJhN3Zh",
+  tiktok: "https://www.tiktok.com/avantmovies",
 };
 
 export const navLinks = [
@@ -27,6 +27,7 @@ export type CatalogueTitle = {
   shortDescription: string;
   artwork: string;
   backdrop: string;
+  heroPreviewUrl?: string;
   legacyPath: string;
   featured?: boolean;
   available?: boolean;
